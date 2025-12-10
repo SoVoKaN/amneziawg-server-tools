2e17c56edf8ac9c45cdec850613fea8def4b5923
SoVoKaN <sovokan@outlook.com>
2025-12-10 17:26:07 +0300

    installer: fix installation on Ubuntu
    The installer previously failed on Ubuntu systems because it assumed that the package index was already up-to-date. The updated code now reliably refreshes the package index before installing the required dependencies.

--- lib/installer/ubuntu/install-packages.sh
+++ lib/installer/ubuntu/install-packages.sh
@@ -5,7 +5,11 @@ install_packages() {
         sed 's/^deb/deb-src/' /etc/apt/sources.list > /etc/apt/sources.list.d/amneziawg.sources.list
     fi
 
+    apt-get update
     apt-get install -y software-properties-common linux-headers-$(uname -r)
+
     add-apt-repository -y ppa:amnezia/ppa
+
+    apt-get update
     apt-get install -y ${INSTALLATION_PACKAGES}
 }
