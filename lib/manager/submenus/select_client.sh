ae713bdae4cb83bae78bc56987083e600d41c0e9
SoVoKaN <sovokan@outlook.com>
2026-01-12 01:27:56 +0300

    manager: add interface selection submenu
    Creates a convenient submenu for selecting clients, simplifying dynamic control of multiple AWG clients.

---
+++ lib/manager/submenus/select_client.sh
@@ -0,0 +1,127 @@
+get_awg_all_clients_count() {
+    AWG_CLIENTS_COUNT="0"
+
+    for CLIENT_DATA_PATH in "${AWG_SERVER_TOOLS_PATH}/interfaces/${AWG_INTERFACE_NAME}/clients/"*.data; do
+        if [ ! -f "$CLIENT_DATA_PATH" ]; then
+            return
+        else
+            break
+        fi
+    done
+
+    for CLIENT_DATA_PATH in "${AWG_SERVER_TOOLS_PATH}/interfaces/${AWG_INTERFACE_NAME}/clients/"*.data; do
+        AWG_CLIENTS_COUNT="$((AWG_CLIENTS_COUNT + 1))"
+    done
+}
+
+show_awg_no_all_clients_message() {
+    if [ -n "$AWG_SERVER_TOOLS_PAGER" ]; then
+        printf "No clients have been created yet." | "$AWG_SERVER_TOOLS_PAGER"
+    else
+        echo "No clients have been created yet."
+    fi
+}
+
+generate_select_awg_all_clients_submenu() {
+    SELECT_AWG_CLIENTS_LIST=""
+    SELECT_AWG_CLIENT_SUBMENU=""
+
+    CURRENT_CLIENT_NUMBER="0"
+    for CLIENT_DATA_PATH in "${AWG_SERVER_TOOLS_PATH}/interfaces/${AWG_INTERFACE_NAME}/clients/"*.data; do
+        CURRENT_CLIENT_NUMBER="$((CURRENT_CLIENT_NUMBER + 1))"
+        CURRENT_CLIENT_NAME="${CLIENT_DATA_PATH##*/}"
+        CURRENT_CLIENT_NAME="${CURRENT_CLIENT_NAME%.data}"
+
+        SELECT_AWG_CLIENTS_LIST="${SELECT_AWG_CLIENTS_LIST}${CURRENT_CLIENT_NAME} "
+
+        SELECT_AWG_CLIENT_SUBMENU="${SELECT_AWG_CLIENT_SUBMENU}${CURRENT_CLIENT_NUMBER}) ${CURRENT_CLIENT_NAME}\n"
+    done
+
+    LAST_CLIENT_NUMBER="$CURRENT_CLIENT_NUMBER"
+
+    SELECT_AWG_CLIENT_SUBMENU="${SELECT_AWG_CLIENT_SUBMENU}0) Back\n\n"
+
+    SELECT_AWG_CLIENT_SUBMENU="${SELECT_AWG_CLIENT_SUBMENU}Select interface [0-${LAST_CLIENT_NUMBER}]: "
+}
+
+
+get_awg_clients_count() {
+    case "$SUBMENU_MODE" in
+        "all")
+            get_awg_all_clients_count
+            ;;
+    esac
+}
+
+show_awg_no_clients_message() {
+    case "$SUBMENU_MODE" in
+        "all")
+            show_awg_no_all_clients_message
+            ;;
+    esac
+}
+
+generate_select_awg_clients_submenu() {
+    case "$SUBMENU_MODE" in
+        "all")
+            generate_select_awg_all_clients_submenu
+            ;;
+    esac
+}
+
+set_awg_client_name() {
+    NUM="0"
+    for CURRENT_CLIENT_NAME in $SELECT_AWG_CLIENTS_LIST; do
+        NUM="$((NUM + 1))"
+
+        if [ "$NUM" = "$1" ]; then
+            AWG_CLIENT_NAME="$CURRENT_CLIENT_NAME"
+            break
+        fi
+    done
+}
+
+
+select_awg_client_submenu() {
+    LIMIT_AWG_CLIENTS_EXCEEDED_HANDLER="$1"
+    SUBMENU_MODE="$2"
+
+    get_awg_clients_count
+
+    if [ "$AWG_CLIENTS_COUNT" = "0" ]; then
+        set_awg_server_tools_pager
+
+        show_awg_no_clients_message
+
+        return 1
+    fi
+
+    if [ "$AWG_CLIENTS_COUNT" -gt 15 ]; then
+        "$LIMIT_AWG_CLIENTS_EXCEEDED_HANDLER"
+        return
+    fi
+
+    generate_select_awg_clients_submenu
+
+    while :; do
+        printf "${SELECT_AWG_CLIENT_SUBMENU}"
+
+        handle_user_input
+
+        clean_lines "$((LAST_CLIENT_NUMBER + 3))"
+
+        case "$USER_INPUT" in
+            [1-9] | [1-9][0-9])
+                if [ "$USER_INPUT" -gt "$LAST_CLIENT_NUMBER" ]; then
+                    continue
+                fi
+
+                set_awg_client_name "$USER_INPUT"
+                break
+                ;;
+            "0")
+                return 1
+                ;;
+        esac
+    done
+}
