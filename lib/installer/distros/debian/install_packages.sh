6d6b938ef5091691e41c2858ccb75bc063532fa3
SoVoKaN <sovokan@outlook.com>
2026-02-14 03:23:39 +0300

    installer: select Ubuntu suite based on Debian version
    Debian installer now chooses the correct Ubuntu suite when adding the PPA based on the Debian distribution version.

--- lib/installer/distros/debian/install_packages.sh
+++ lib/installer/distros/debian/install_packages.sh
@@ -18,9 +18,18 @@ install_packages() {
 
     curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x57290828" | gpg --dearmor --output "/etc/apt/keyrings/amneziawg-keyring.gpg"
 
+    case "$VERSION_ID" in
+        "11" | "12")
+            UBUNTU_SUITE="focal"
+            ;;
+        "13")
+            UBUNTU_SUITE="jammy"
+            ;;
+    esac
+
     echo "Types: deb deb-src
 URIs: https://ppa.launchpadcontent.net/amnezia/ppa/ubuntu
-Suites: focal
+Suites: ${UBUNTU_SUITE}
 Components: main
 Signed-By: /etc/apt/keyrings/amneziawg-keyring.gpg
 " > "/etc/apt/sources.list.d/amneziawg.sources"
